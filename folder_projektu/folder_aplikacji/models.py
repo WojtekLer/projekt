11cf40b517ac89ccef46af767417560a470816ba
Wojtek <wstepniewski@st.swps.edu.pl>
2025-01-20 10:38:35 +0100

    zadania_lab_03

--- folder_projektu/folder_aplikacji/models.py
+++ folder_projektu/folder_aplikacji/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
 
@@ -22,6 +21,7 @@ class Team(models.Model):
 class Person(models.Model):
 
     name = models.CharField(max_length=60)
+    pseudonim = models.CharField(max_length=80, default="")
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
     month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
